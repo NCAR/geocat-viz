dc2f1bfdbbcaa779e05ebe9d613d478de971664a
Kevin Hallock <hallock@ucar.edu>
2020-01-24 09:44:44 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 with open("src/_version.py") as f:
     exec(f.read())
 
-setup(name='geocat.viz',
-      version=__version__,
-      url='https://github.com/NCAR/geocat-viz',
-      author='GeoCAT',
-      author_email='geocat@ucar.edu',
-      license='Apache 2.0',
-      packages=['geocat.viz'],
-      package_dir={'geocat.viz': 'src'},
-      namespace_packages=['geocat'],
-      zip_safe=False)
+setup(
+    name='geocat.viz',
+    version=__version__,
+    url='https://github.com/NCAR/geocat-viz',
+    author='GeoCAT',
+    author_email='geocat@ucar.edu',
+    license='Apache 2.0',
+    packages=['geocat.viz'],
+    package_dir={'geocat.viz': 'src'},
+    namespace_packages=['geocat'],
+    zip_safe=False,
+)
