70bed4280a94df7d6478b10fbe680b499b879ba5
erogluorhan <erogluorhan@gmail.com>
2020-02-12 15:23:24 -0700

    set_titles_and_labels() parameter names and docstrings updated to address a review

--- src/util.py
+++ src/util.py
@@ -65,38 +65,53 @@ def add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=3, labelsiz
         right=True,
     )
 
-def set_titles_and_labels(ax, title=None, titlefontsize=18, left=None, leftfontsize=18, right=None, rightfontsize=18,
+def set_titles_and_labels(ax, maintitle=None, maintitlefontsize=18, lefttitle=None, lefttitlefontsize=18, righttitle=None, righttitlefontsize=18,
                           xlabel=None, ylabel=None, labelfontsize=16):
     """
-    Utility function to handle axis titles, left/right titles, and labels
+    Utility function to handle axis titles, left/right aligned titles, and labels as they appear in NCL plots.
+
+    (1) If no lefttitle and righttitle is set, maintitle is placed just top to the axes as follows:
+
+                     maintitle
+     ___________________________________________
+    |                   Axes                    |
+    |                                           |
+
+    (2) If any of lefttitle or righttitle is set, lefttitle and righttitle are placed into a row that is just top to the axes, and maintitle is placed to top of
+    the row of lefttitle/righttitle as follows:
+
+                     maintitle
+     lefttitle                        righttitle
+     ___________________________________________
+    |                   Axes                    |
+    |                                           |
 
     Args:
 
         ax (:class:`matplotlib.axes._subplots.AxesSubplot` or :class:`cartopy.mpl.geoaxes.GeoAxesSubplot`):
             Current axes to the current figure
 
-        title (:class:`str`):
-            Text to use for the Title. If there is no left or right titles given, Title is placed just top of the
-            axes. If any of left or right titles is given, Title is placed to top of the left/right title row.
+        maintitle (:class:`str`):
+            Text to use for the Title.
 
-        titlefontsize (:class:`int`):
+        maintitlefontsize (:class:`int`):
             Title text font size. A default value of 18 is used if nothing is set.
 
-        left (:class:`str`):
+        lefttitle (:class:`str`):
             Text to use for an optional left-aligned title, if any. For most plots, only a Title is enough,
             but for some plot types, a left-aligned likely with a  right-aligned title can be used together. If a
             left-aligned title as well as regular Title are set together, Title is placed to top of the left/right title row.
 
-        leftfontsize (:class:`int`):
+        lefttitlefontsize (:class:`int`):
             Left-aligned title text font size. A default value of 18 is used if nothing is set.
 
-        right (:class:`str`):
+        righttitle (:class:`str`):
             Text to use for an optional right-aligned title, if any. For most plots, only a Title is enough,
             but for some plot types, a right-aligned likely with a left-aligned title can be used together. If a
             right-aligned title as well as regular Title are set together, Title is placed to top of the left/right
             title row.
 
-        rightfontsize (:class:`int`):
+        righttitlefontsize (:class:`int`):
             Right-aligned title text font size. A default value of 18 is used if nothing is set.
 
         xlabel (:class:`str`):
@@ -109,17 +124,17 @@ def set_titles_and_labels(ax, title=None, titlefontsize=18, left=None, leftfonts
             Text font size for x- and y-axes. A default value of 16 is used if nothing is set.
     """
 
-    if title is not None:
-        if left is not None or right is not None:
-            ax.set_title(title, fontsize=titlefontsize+2, y=1.12)
+    if maintitle is not None:
+        if lefttitle is not None or righttitle is not None:
+            ax.set_title(maintitle, fontsize=maintitlefontsize + 2, y=1.12)
         else:
-            ax.set_title(title, fontsize=titlefontsize, y=1.04)
+            ax.set_title(maintitle, fontsize=maintitlefontsize, y=1.04)
 
-    if left is not None:
-        ax.set_title(left, fontsize=leftfontsize, y=1.04, loc='left')
+    if lefttitle is not None:
+        ax.set_title(lefttitle, fontsize=lefttitlefontsize, y=1.04, loc='left')
 
-    if right is not None:
-        ax.set_title(right, fontsize=rightfontsize, y=1.04, loc='right')
+    if righttitle is not None:
+        ax.set_title(righttitle, fontsize=righttitlefontsize, y=1.04, loc='right')
 
     if xlabel is not None:
         ax.set_xlabel(xlabel, fontsize=labelfontsize)
