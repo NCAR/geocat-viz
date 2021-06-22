0339092fd9b23141fb796cb1069f11d25b0ce62b
anissa111 <anissaz@ucar.edu>
2021-06-22 14:36:54 -0600

    fixed version stuff?

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-with open("src/_version.py") as f:
-    exec(f.read())
+with open('README.md') as f:
+    long_description = f.read()
+
+
+# ''' moved into function, can now be used other places
+def version():
+    for line in open('meta.yaml').readlines():
+        index = line.find('set version')
+        if index > -1:
+            return line[index + 15:].replace('\" %}', '').strip()
+
 
 setup(
     name='geocat.viz',
-    version=__version__,
+    version=version(),
     url='https://github.com/NCAR/geocat-viz',
     author='GeoCAT',
     author_email='geocat@ucar.edu',
