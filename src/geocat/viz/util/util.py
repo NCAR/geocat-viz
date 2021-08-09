7d6126558767479a2715da7fadf0d2df57f121b0
Jiaqi Li <jiaqili1620@gmail.com>
2021-08-09 15:50:19 -0600

    minor update

--- src/geocat/viz/util/util.py
+++ src/geocat/viz/util/util.py
@@ -1,18 +1,20 @@
 def set_tick_direction_spine_visibility(ax,
-                                        tickmark_direction='out',
+                                        tick_direction='out',
                                         top_spine_visible=True,
                                         bottom_spine_visible=True,
                                         left_spine_visible=True,
                                         right_spine_visible=True):
     """
-    Utility function to turn off axes spines and set tickmark orientations
+    Utility function to turn off axes spines and set tickmark orientations.
+    
+    Note: This function should be called after calling add_major_minor_ticks()
 
     Args:
 
         ax (:class:`matplotlib.axes._subplots.AxesSubplot` or :class:`cartopy.mpl.geoaxes.GeoAxesSubplot`):
             Current axes to the current figure
 
-        tick_mark_direction (:class:`str`):
+        tick_direction (:class:`str`):
             Set 'in' to put ticks inside the axes, 
             'out' to put ticks outside the axes,
             'inout' to put ticks both in and out of the axes.
@@ -29,7 +31,7 @@ def set_tick_direction_spine_visibility(ax,
         right_spine_visible (:class:`bool`):
             Set False to turn off right spine.
     """
-    ax.tick_params(direction=tickmark_direction, axis='both', which='both')
+    ax.tick_params(direction=tick_direction, axis='both', which='both')
     ax.spines['top'].set_visible(top_spine_visible)
     ax.spines['bottom'].set_visible(bottom_spine_visible)
     ax.spines['left'].set_visible(left_spine_visible)
@@ -37,13 +39,12 @@ def set_tick_direction_spine_visibility(ax,
 
     if top_spine_visible and bottom_spine_visible:
         ax.xaxis.set_ticks_position('default')
-    elif bottom_spine_visible and (not top_spine_visible):
+    elif bottom_spine_visible and not top_spine_visible:
         ax.xaxis.set_ticks_position('bottom')
-    elif top_spine_visible and (not bottom_spine_visible):
+    elif top_spine_visible and not bottom_spine_visible:
         ax.xaxis.set_ticks_position('top')
     else:
         ax.xaxis.set_ticks_position('none')
-        #ax.get_xaxis().set_visible(False)
 
     if left_spine_visible and right_spine_visible:
         ax.yaxis.set_ticks_position('default')
@@ -53,7 +54,6 @@ def set_tick_direction_spine_visibility(ax,
         ax.yaxis.set_ticks_position('right')
     else:
         ax.yaxis.set_ticks_position('none')
-        #ax.get_yaxis().set_visible(False)
 
 
 def add_lat_lon_ticklabels(ax,
