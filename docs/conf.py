a34e95b4a20df6fc51319746c186624f37047968
erinlincoln <erin_lincoln@brown.edu>
2021-09-26 17:34:38 -0600

    Attempting to add clickable typing

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,22 @@ import sys
 
 import geocat.viz
 
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ["xarray"]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- Project information -----------------------------------------------------
 
 project = 'GeoCAT-viz'
