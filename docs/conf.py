9cf0659af1d704860652de47b2f4c9c29dc7c014
erinlincoln <erin_lincoln@brown.edu>
2021-10-18 14:49:34 -0600

    Attempt to hide init on Taylor diagram

--- docs/conf.py
+++ docs/conf.py
@@ -51,9 +51,9 @@ napoleon_use_admonition_for_examples = True
 napoleon_include_special_with_doc = True
 
 autosummary_generate = True
-autoclass_content = 'class'
+#autoclass_content = 'class'
 
-autodoc_default_flags = ['members', 'inherited-members', 'show-inheritance']
+#autodoc_default_flags = ['members', 'inherited-members', 'show-inheritance']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
