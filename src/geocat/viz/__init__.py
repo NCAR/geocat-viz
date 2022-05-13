12dc5521f735651aaefe4e8ed32eca4d7d5c8436
anissa111 <anissaz@ucar.edu>
2022-05-13 12:29:09 -0600

    do an extremely silly thing

--- src/geocat/viz/__init__.py
+++ src/geocat/viz/__init__.py
@@ -2,3 +2,4 @@ from .contourf import *
 from .plot_util import *
 from .taylor import *
 from .util import *
+from .cmaps import *
---
+++ src/geocat/viz/cmaps.py
@@ -0,0 +1,5 @@
+# silly import for backwards compatibility
+import warnings
+import cmaps
+
+warnings.warn("geocat.viz.cmaps is deprecated, use cmaps instead", DeprecationWarning)
