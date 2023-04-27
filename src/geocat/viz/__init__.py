17216ca7c49da861cc4bae1829f9f76f656171ed
Julia Kent <46687291+jukent@users.noreply.github.com>
2023-04-27 15:00:45 -0600

    Update __init__.py

--- src/geocat/viz/__init__.py
+++ src/geocat/viz/__init__.py
@@ -4,7 +4,7 @@ from .util import *
 # get version from pyproject.toml
 from importlib.metadata import version as _version
 try:
-    __version__ = _version("geocat.comp")
+    __version__ = _version("geocat.viz")
 except Exception:
     # Local copy or not installed with setuptools.
     # Disable minimum version checks on downstream libraries.
