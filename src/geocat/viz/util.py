54ca5c31feda34b8b3af137de03d1e99928c8da4
Julia Kent <46687291+jukent@users.noreply.github.com>
2023-05-12 09:28:32 -0600

    do deprecations how comp does

--- src/geocat/viz/util.py
+++ src/geocat/viz/util.py
@@ -863,7 +863,9 @@ def findLocalExtrema(da: xr.DataArray,
                      lowVal: int = 1000,
                      eType: str = 'Low',
                      eps: float = 10) -> list:
-    """This function is deprecated. Call `find_local_extrema` instead.
+    r""".. deprecated:: 2023.02.0 The ``findLocalExtrema`` function is deprecated due to
+    naming conventions. Use `find_local_extrema <https://geocat-viz.readthedocs.io/en/stable/user_api/generated/geocat.viz.util.find_local_extrema.html>`__
+    instead.
 
     Utility function to find local low/high field variable coordinates on a
     contour map. To classify as a local high, the data point must be greater
@@ -1041,7 +1043,9 @@ def plotCLabels(ax: matplotlib.axes.Axes,
                 fontsize: int = 12,
                 whitebbox: bool = False,
                 horizontal: bool = False) -> list:
-    """This function is deprecated. Call `plot_contour_labels` instead.
+    r""".. deprecated:: 2023.02.0 The ``plotCLabels`` function is deprecated due to
+    naming conventions. Use `plot_contour_levels <https://geocat-viz.readthedocs.io/en/stable/user_api/generated/geocat.viz.util.plot_contour_levels.html>`__
+    instead.
 
     Utility function to plot contour labels by passing in a coordinate to
     the clabel function.
@@ -1191,7 +1195,9 @@ def plotELabels(da: xr.DataArray,
                 fontsize: int = 22,
                 whitebbox: bool = False,
                 horizontal: bool = True) -> list:
-    """This function is deprecated. Please use `plot_extrema_labels` instead.
+    r""".. deprecated:: 2023.02.0 The ``plotELabels`` function is deprecated due to
+    naming conventions. Use `plot_extrema_labels <https://geocat-viz.readthedocs.io/en/stable/user_api/generated/geocat.viz.util.plot_extrema_labels.html>`__
+    instead.
 
     Utility function to plot high/low contour labels.
 
