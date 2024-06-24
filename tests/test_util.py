a7449b0c85d102f747a555c6922f29cf1bf4eea3
Julia Kent <46687291+jukent@users.noreply.github.com>
2024-06-24 10:06:26 -0600

    add tests so far

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,13 +1,146 @@
 import pytest
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import xarray as xr
 
-from geocat.viz.util import truncate_colormap
+
+import geocat.viz.util as gv
+import geocat.datafiles as gdf
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_set_tick_direction_spine_visibility():
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    gv.set_tick_direction_spine_visibility(ax,
+                                        tick_direction='in',
+                                        top_spine_visible=False,
+                                        right_spine_visible=False)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_add_lat_lon_gridlines():
+    fig = plt.figure(figsize=(10, 10))
+
+    ax = plt.axes(projection=ccrs.NorthPolarStereo(central_longitude=10))
+    ax.set_extent([4.25, 15.25, 42.25, 49.25], ccrs.PlateCarree())
+
+    gl = gv.add_lat_lon_gridlines(
+        ax,
+        color='black',
+        labelsize=14,
+        xlocator=np.arange(4, 18, 2),  # longitudes for gridlines
+        ylocator=np.arange(43, 50))  # latitudes for gridlines
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_add_right_hand_axis():
+    fig = plt.figure(figsize=(9, 10))
+    ax1 = plt.gca()
+
+    gv.add_right_hand_axis(ax1,
+                        label="Right Hand Axis",
+                        ylim=(0, 13),
+                        yticks=np.array([4, 8]),
+                        ticklabelsize=15,
+                        axislabelsize=21)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_add_height_from_pressure_axis():
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes()
+    plt.yscale('log')
+    ax.invert_yaxis()
+
+    gv.add_height_from_pressure_axis(ax, heights=[4, 8])
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_add_lat_lon_ticklabels():
+    fig = plt.figure(figsize=(12, 6))
+
+    projection = ccrs.PlateCarree()
+    ax = plt.axes(projection=projection)
+    ax.add_feature(cfeature.LAND, color='silver')
+
+    gv.add_lat_lon_ticklabels(ax)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_add_major_minor_ticks():
+    fig = plt.figure(figsize=(12, 6))
+    ax = plt.axes()
+
+    gv.add_major_minor_ticks(ax,
+                            x_minor_per_major=4,
+                            y_minor_per_major=5,
+                            labelsize="small")
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_set_titles_and_labels():
+    fig = fig, ax = plt.subplots()
+
+    gv.set_titles_and_labels(ax,
+                            maintitle="Title",
+                            maintitlefontsize=24,
+                            subtitle="Subtitle",
+                            xlabel="x",
+                            ylabel="y",
+                            labelfontsize=16)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.02,
+                               remove_text=True,
+                               style='default')
+def test_set_axes_limits_and_ticks():
+    fig = fig, ax = plt.subplots()
+
+    gv.set_axes_limits_and_ticks(ax,
+                                xlim=(0, 900),
+                                ylim=(100, 1000),
+                                xticks=range(0, 901, 100),
+                                yticks=range(100, 1001, 100))
+    return fig
 
 
 def test_truncate_colormap():
     cmap = mpl.colormaps['terrain']
-    truncated_cmap = truncate_colormap(cmap, 0.1, 0.9)
+    truncated_cmap = gv.truncate_colormap(cmap, 0.1, 0.9)
 
     assert isinstance(truncated_cmap, mpl.colors.LinearSegmentedColormap)
     assert truncated_cmap(0.0) == cmap(0.1)
     assert truncated_cmap(1.0) == cmap(0.9)
+
+
+#def test_xr_add_cyclic_longitudes():
+#    ds = xr.open_dataset(gdf.get("netcdf_files/uv300.nc")).isel(time=1)
+#    U = ds.U
+#
+#    U = gv.xr_add_cyclic_longitudes(U, 'lon')
+#    assert U.lon == 
